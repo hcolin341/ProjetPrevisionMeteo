d8e2c09c7c38020b76911aee694df74bee3a8b4d
hcolin341 <haylann02@gmail.com>
2022-05-12 17:41:20 +0200

    separationdesfonctions_domevent_etmiseencache

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
-function recuperation_donnees   ()
-{
-    const VILLE = document.getElementById("ville").value;
-    const NOMBRE = document.getElementById("NombreJours").value;
-    const APIGEOLOCATE_KEY = "4cc84d243e404697a08e5534056dbde4"
-    const APIWEATHER_KEY = "d5feee14cc21f117d2a02f17f9c1e44e"
-    let URLG = `https://api.opencagedata.com/geocode/v1/json?q=${VILLE}&key=${APIGEOLOCATE_KEY}&language=fr&pretty=1`
-    
-    let ladate = new Date;
-    let lasemaine = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi","Vendredi", "Samedi"]
-    let indexjour = ladate.getDay(); 
+document.addEventListener("DOMContentLoaded", function() {
 
-    fetch(URLG) 
+const bouttonFonction = document.getElementById("soumettre");
+
+bouttonFonction.addEventListener("click", (event) => {
+    event.preventDefault();
+    recuperation_donnees();
+})})
+
+function recuperation_donnees()
+{
+    const ville = document.getElementById("ville").value;
+    const nombre = document.getElementById("NombreJours").value;
+    const apigeolocate_key = "4cc84d243e404697a08e5534056dbde4"
+    const apiweather_key = "d5feee14cc21f117d2a02f17f9c1e44e"
+
+    monstockage = localStorage; // init du stockage local (non necessaire)
+
+    var lavilledonnee = monstockage.getItem(ville);
+
+    if(lavilledonnee)
+    {
+        var meteos = JSON.parse(lavilledonnee);
+        displayWeather(meteos, nombre);
+        console.log("elle y est deja")
+    }
+    else
+    {
+        getGeolocateFromApi(ville, apigeolocate_key)
+        .then(data => getWeatherFromApi(data.lat, data.lon, nombre, ville, monstockage, apiweather_key))
+        .then(donnees => displayWeather(donnees, nombre))
+        console.log("elle y etait pas")
+    }
+}
+
+function getGeolocateFromApi(ville, apigeolocate_key)
+{
+    let UrlGeolocate = `https://api.opencagedata.com/geocode/v1/json?q=${ville}&key=${apigeolocate_key}&language=fr&pretty=1`
+    return fetch(UrlGeolocate) 
     .then(response => { 
     if (response.status == 200)
     { 
-        return response.json() 
+        return response.json(); 
     }
     else console.log(`Erreur lorsqu'on a tenté de récupérer les data`); 
     })
-    .then(data => {
-    const LAT = data.results[0].geometry.lat; 
-    const LON = data.results[0].geometry.lng;
-    let URLW = `https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&exclude={current,minutely,hourly,alerts}&appid=${APIWEATHER_KEY}`
-    console.log(URLW);
-    fetch(URLW)
+    .then(data => 
+    {   
+        const lat = data.results[0].geometry.lat; 
+        const lon = data.results[0].geometry.lng;
+
+        const position = {lat, lon};
+        return position;
+    }
+    )
+    .catch(err => console.log(err))
+}
+
+function getWeatherFromApi(lat, lon, nombre, ville, monstockage, apiweather_key)
+{
+
+    let urlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,hourly,alerts}&appid=${apiweather_key}`
+    
+    return fetch(urlWeather)
     .then(reponse => { 
     if (reponse.status == 200) { 
         return reponse.json()  
@@ -31,25 +69,62 @@ function recuperation_donnees   ()
     else console.log(`Erreur lorsqu'on a tenté de récupérer les data`); 
     })
     .then(donnee => {
-    resultats.innerHTML = "";
-    var code_meteo;
-      for (var i = 0; i < NOMBRE; i++)
+    
+    var meteos = []; 
+      for (var i = 0; i < 7; i++)
     {
-        code_meteo = donnee.daily[i].weather[0].id;      
-        if(code_meteo == 800)
-           imgSrc = "./images/sun.svg";
+        meteos[i] = donnee.daily[i].weather[0].id;  // recuperation du code meteo dans le retour du fetch
+    }
 
-        if(code_meteo >= 200 && code_meteo <= 531)
-            imgSrc = "./images/rain.svg";
+    monstockage.setItem(ville, JSON.stringify(meteos)) // Ajoute le tableau Meteos dans la base de donnees locales 
 
-        if(code_meteo == "")
-            imgSrc =  "./images/cloudy.svg"
+    const illumi = donnee.current.uvi;
 
-        if( code_meteo < 803 && code_meteo > 800)
-            imgSrc =  "./images/cloudy.svg"
+    const corps = document.getElementById("corps");
+    const result = document.getElementById("resultats");
 
-        if( code_meteo == 803 || code_meteo == 804)
-            imgSrc = "./images/clouds.svg"
+    if (illumi == 0)
+    {
+        corps.style["background-color"] = "";
+        corps.style["background"] = "linear-gradient(0deg, #003AFF, #010F40)";
+        result.style["color"] = "white";
+    }
+    else 
+    {
+        corps.style["background"] = "";
+        corps.style["background-color"] = "rgba(25, 140, 255, 0.725)"
+        result.style["color"] = "black";
+    }
+    
+    return meteos;
+})}
+
+function displayWeather(code_meteo, nombre)
+{
+    resultats.innerHTML = "";
+    
+    //var code_meteo = JSON.parse(lavilledonnee);
+    //console.log(code_meteo)
+
+    let ladate = new Date;
+    let lasemaine = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi","Vendredi", "Samedi"]
+    let indexjour = ladate.getDay(); 
+
+    var imagSrc;
+
+    for (var i = 0; i < nombre; i++)
+    {
+        if(code_meteo[i] == 800)
+           imagSrc = "./images/sun.svg";
+
+        if(code_meteo[i] >= 200 && code_meteo[i] <= 531)
+            imagSrc = "./images/rain.svg";
+
+        if(code_meteo[i] < 803 && code_meteo[i] > 800)
+            imagSrc =  "./images/cloudy.svg"
+
+        if(code_meteo[i] == 803 || code_meteo[i] == 804)
+            imagSrc = "./images/clouds.svg"
 
         lejour = lasemaine[indexjour];
 
@@ -58,7 +133,7 @@ function recuperation_donnees   ()
         let h = document.createElement("H3");
         let t = document.createTextNode(lejour);
         let img = document.createElement("img");
-        img.src = imgSrc;
+        img.src = imagSrc;
 
         h.appendChild(t)
         d.appendChild(h);
@@ -70,23 +145,5 @@ function recuperation_donnees   ()
         {  indexjour = 0; }
         else 
             indexjour++;
-
-        const illumi = donnee.current.uvi;
-        const DOCU = document.getElementById("tout");
-        const CORPS = document.getElementById("corps");
-        if (illumi == 0)
-        {
-            CORPS.style["background-color"] = "";
-            CORPS.style["background"] = "linear-gradient(0deg, #003AFF, #010F40)";
-            console.log("test");    
-        }
-        else 
-        {
-            CORPS.style["background"] = "";
-            CORPS.style["background-color"] = "rgba(25, 140, 255, 0.725)"
-        }
     }
-    })
-    })
-    .catch(err => console.log(err))
-}      
+}
